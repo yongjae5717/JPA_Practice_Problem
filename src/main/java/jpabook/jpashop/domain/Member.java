748759eabf94e3d5721e55e2612bb6ebfca12888
yongjae5717 <dydwo5717@naver.com>
2022-09-08 20:40:29 +0900

    add Entity & Basic Mapping

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,2 +1,60 @@
-package jpabook.jpashop.domain;public class Member {
+package jpabook.jpashop.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "member_id", nullable = false)
+    private Long id;
+
+    private String name;
+
+    private String city;
+
+    private String street;
+
+    private String zipcode;
+
+    //예제이므로 getter & setter을 만들어줌
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
 }
--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,2 +1,61 @@
-package jpabook.jpashop.domain;public class OrderItem {
+package jpabook.jpashop.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "order_item_id", nullable = false)
+    private Long id;
+
+    @Column(name = "order_id")
+    private Long orderId;
+
+    @Column(name = "item_id")
+    private Long itemId;
+
+    private int orderPrice;
+
+    private int count;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public Long getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(Long itemId) {
+        this.itemId = itemId;
+    }
+
+    public int getOrderPrice() {
+        return orderPrice;
+    }
+
+    public void setOrderPrice(int orderPrice) {
+        this.orderPrice = orderPrice;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
 }
